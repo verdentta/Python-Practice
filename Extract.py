8b240ed118517ffcb84d85811e86250e5f5cf9d1
Romeo Paul <verdentta@gmail.com>
2024-08-27 12:27:10 -0400

    fixed a small error

--- Extract.py
+++ Extract.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service 
 
-from selenium.webdriver.common.by import By 4
+from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 
